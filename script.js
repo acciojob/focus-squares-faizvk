cf4c4435dad40fee2e12c5dcd686b880728f4383
Faiz Zubair <faizvk14@gmail.com>
2026-01-29 00:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//your JS code here. If required.
 const squares = document.querySelectorAll(".square");
 
 const lavender = "#E6E6FA";
 const coffee = "#6F4E37";
 
 squares.forEach(activeSquare => {
-  activeSquare.addEventListener("mouseenter", () => {
+  activeSquare.addEventListener("mouseover", () => {
     squares.forEach(square => {
       if (square !== activeSquare) {
         square.style.backgroundColor = coffee;
@@ -13,7 +12,7 @@ squares.forEach(activeSquare => {
     });
   });
 
-  activeSquare.addEventListener("mouseleave", () => {
+  activeSquare.addEventListener("mouseout", () => {
     squares.forEach(square => {
       square.style.backgroundColor = lavender;
     });
